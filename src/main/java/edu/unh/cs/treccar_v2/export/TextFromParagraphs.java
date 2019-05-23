363ec6aa6a705c8a6056a6adc49a2d1dc672ed70
Laura Dietz <dietz@cs.umass.edu>
2019-05-23 16:16:34 -0400

    Example code to extract text from paragraphs

---
+++ src/main/java/edu/unh/cs/treccar_v2/export/TextFromParagraphs.java
@@ -0,0 +1,29 @@
+package edu.unh.cs.treccar_v2.export;
+
+import edu.unh.cs.treccar_v2.Data;
+import edu.unh.cs.treccar_v2.read_data.DeserializeData;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/**
+ * Get text from all paragraphs in paragraphCorpus.cbor
+ * User: dietz
+ * Date: 5/23/19
+ * Time: 4:06 PM
+ */
+public class TextFromParagraphs {
+  public static void main(String[] args) throws FileNotFoundException {
+    System.setProperty("file.encoding", "UTF-8");
+    final FileInputStream fileInputStream = new FileInputStream(new File(args[0]));
+
+    for(Data.Paragraph para: DeserializeData.iterableParagraphs(fileInputStream)) {
+      String paraId = para.getParaId();
+      String paraText = para.getTextOnly();
+      System.out.println( paraId + "\t" + paraText);
+    }
+
+  }
+
+}
